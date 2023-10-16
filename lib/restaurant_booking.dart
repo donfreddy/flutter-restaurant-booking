8670f4767f6a3e596c235769539f333fb235de87
Don Freddy <freddytamwo@gmail.com>
2023-10-16 12:52:26 +0100

    first commit

---
+++ lib/restaurant_booking.dart
@@ -0,0 +1,16 @@
+library restaurant_booking;
+
+import 'package:flutter/material.dart';
+
+class RestaurantBooking {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Restaurant Booking'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Restaurant Booking Mini-program!'),
+      ),
+    );
+  }
+}
